abee70bde006a4d93178b407c98fcafb8cef929c
Arbaz009 <arbazansari78692s@gmail.com>
2025-01-03 23:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT;
 //   try {
 //     const connection = await mongoose.connect(`${process.env.MONGODB_URI}`);
 //     console.log("Connected to database with :", connection.connection.host);
-
 //     app.listen(PORT, () => {
 //       console.log(`Server is running on port ${PORT}`);
 //     });
@@ -20,85 +19,6 @@ const PORT = process.env.PORT;
 //   }
 // })();
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: [{
-      title: {
-        type: String,
-      },
-      description: {
-        type: String,
-      },
-      logo: {
-        type: String,
-      },
-      url: {
-        type: String,
-      },
-      id: {
-        type: String,
-      },
-    }],
-    default: [],
-  },
-});
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    if (user.password !== password) {
-      return res.status(401).json({ message: "Invalid password" });
-    }
-
-    res.json({ message: "Login successful", data: user });
-  } catch (error) {
-    res.status(500).json({ message: "Error logging in", error });
-  }
-});
-
-
-
-const User = mongoose.model("User", userSchema);
-
-app.use(express.json());
-
-app.post("/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const user = await User({ name, email, password });
-
-    const saveduser = await user.save();
-
-    res.json({ message: "User created successfully", data: saveduser });
-  } catch (error) {
-    res.status(500).json({ message: "Failed to create user", error });
-  }
-});
-
-// Get all users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+} );
