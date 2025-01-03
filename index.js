9fc965502a15b736a2bd83c852bc1bf075960e77
Arbaz009 <arbazansari78692s@gmail.com>
2025-01-03 23:13:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const PORT = process.env.PORT;
 //     process.exit(1);
 //   }
 // })();
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 } );
